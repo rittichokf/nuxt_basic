52d3f1c895d3b8187c73fbe5bab10c9fb693da4d
rittichokf <rittichok.fai@mtsc.co.th>
2025-07-22 23:03:04 +0700

    Initialize Nuxt.js application with essential configuration files and assets
    - Created package.json with Nuxt.js and Vue.js dependencies
    - Added favicon.ico for the application
    - Included robots.txt to manage web crawler access
    - Set up TypeScript configuration with references to Nuxt.js generated files

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/rittichokf/learn/nuxt_tutorial.js/nuxt_basic/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>Welcome to Nuxt</h2>
+  </div>
+</template>
